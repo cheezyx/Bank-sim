96260493d852a9a8e07de7c39613f3d9e6620fd2
yuudachi13 <t2pupa00@students.oamk.fi>
2023-11-09 02:05:25 +0200

    toimiiko4?

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -10,7 +10,7 @@ const cards={
     return db.query('select * from cards where card_id=?', [id], callback);
   },
   add: function(cards, callback) {
-    bcrypt.hash(cards.password, saltRounds, function(err, hash) {
+    bcrypt.hash(cards.pin_hashed, saltRounds, function(err, hash) {
       return db.query('insert into cards (customer_id, card_type, pin_hashed) values(?,?,?)',
       [cards.customer_id, cards.card_type, hash], callback);
     });
@@ -19,7 +19,7 @@ const cards={
     return db.query('delete from cards where card_id=?', [id], callback);
   },
   update: function(id, cards, callback) {
-    bcrypt.hash(cards.password, saltRounds, function(err, hash) {
+    bcrypt.hash(cards.pin_hashed, saltRounds, function(err, hash) {
       return db.query('update cards set customer_id=?, card_type=?, pin_hashed=? where card_id=?',
       [cards.customer_id, cards.card_type, hash, id], callback);
     });
