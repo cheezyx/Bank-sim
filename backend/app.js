84d80ef0b518bb171dfd851a278df49d3b20e44e
cheezyx <cheezyx@users.noreply.github.com>
2023-11-10 02:00:14 +0200

    add account routes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 var indexRouter = require('./routes/index');
 var customersRouter = require('./routes/customers');
 var cardsRouter = require('./routes/cards');
-
+var accountsRouter = require('./routes/accounts');
 
 var app = express();
 
@@ -20,6 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/customers', customersRouter);
 app.use('/cards', cardsRouter);
-
+app.use('/accounts', accountsRouter);
 
 module.exports = app;
