d3d41a008566e0d62d9fc0e59b3099156cbff70d
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2023-11-14 20:08:36 +0200

    Update cards_model.js

--- backend/models/cards_model.js
+++ backend/models/cards_model.js
@@ -1,30 +1,53 @@
 const db = require('../database');
 const bcrypt = require('bcryptjs');
 
-const saltRounds=10;
-const cards={
-  getAll: function(callback) {
+const saltRounds = 10;
+const cards = {
+  getAll: function (callback) {
     return db.query('select * from cards', callback);
   },
-  getById: function(id, callback) {
+  getById: function (id, callback) {
     return db.query('select * from cards where card_id=?', [id], callback);
   },
-  add: function(cards, callback) {
-    bcrypt.hash(cards.pin_hashed, saltRounds, function(err, hash) {
-      return db.query('insert into cards (customer_id, card_type, pin_hashed) values(?,?,?)',
-      [cards.customer_id, cards.card_type, hash], callback);
+  add: function (card, callback) {
+    bcrypt.hash(card.pin_hashed, saltRounds, function (err, hash) {
+      if (err) {
+        return callback(err);
+      }
+      db.query('insert into cards (customer_id, card_type, pin_hashed) values(?,?,?)',
+        [card.customer_id, card.card_type, hash], function (err, result) {
+          if (err) {
+            return callback(err);
+          }
+          const cardId = result.insertId;
+
+          // Tarkistetaan onko taulukko ja jos on...
+          if (Array.isArray(card.account_ids) && card.account_ids.length > 0) {
+            // Syötetään card_privilegesiin jokainen account_id mitä löytyy (tässä tietenkin vain yksi tai kaksi)
+            card.account_ids.forEach(account_id => {
+              db.query('insert into card_privileges (card_id, account_id) values(?,?)',
+                [cardId, account_id], function (err) {
+                  if (err) {
+                    return callback(err);
+                  }
+                });
+            });
+          }
+
+          callback(null, result);
+        });
     });
   },
-  delete: function(id, callback) {
+  delete: function (id, callback) {
     return db.query('delete from cards where card_id=?', [id], callback);
   },
-  update: function(id, cards, callback) {
-    bcrypt.hash(cards.pin_hashed, saltRounds, function(err, hash) {
+  update: function (id, cards, callback) {
+    bcrypt.hash(cards.pin_hashed, saltRounds, function (err, hash) {
       return db.query('update cards set customer_id=?, card_type=?, pin_hashed=? where card_id=?',
-      [cards.customer_id, cards.card_type, hash, id], callback);
+        [cards.customer_id, cards.card_type, hash, id], callback);
     });
   }
 
 }
-          
+
 module.exports = cards;
