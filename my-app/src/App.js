8fea2ed6069666b13f68aad9b762ecad94e3d664
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2023-11-21 21:25:19 +0200

    joko löytys

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage'; // Jos LoginPage.js on pages-kansiossa
+import HomePage from './pages/HomePage';   // Jos HomePage.js on pages-kansiossa
 // Muut import-lauseet
 
 function App() {
