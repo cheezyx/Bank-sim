e45fae0292d99ee9d74e0695af55aa6d2ed20076
cheezyx <cheezyx@users.noreply.github.com>
2023-11-25 17:32:06 +0200

    logouttia ja nappuloita näperretty

--- bank-automat/automat.cpp
+++ bank-automat/automat.cpp
@@ -6,9 +6,63 @@ automat::automat(QWidget *parent) :
     ui(new Ui::automat)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 automat::~automat()
 {
     delete ui;
 }
+
+void automat::setCard_id(const QString &newCardID)
+{
+    cardID = newCardID;
+}
+void automat::showCardID()
+{
+    ui->IDLabelcard->setText(cardID);
+}
+
+
+void automat::on_siirto_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+void automat::on_talletus_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+void automat::on_nosto_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+void automat::on_saldo_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+void automat::on_tilitapahtumat_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+}
+void automat::on_logout_clicked()
+{
+    this->hide();
+    emit logOutSignal();
+    qDebug()<<"Logging out";
+
+}
+
+void automat::on_btnback_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+    ui->screenlabel->clear();
+}
+
+void automat::setToken(const QByteArray &newToken)
+{
+    token = newToken;
+    qDebug()<<token;
+}
+
+
+
