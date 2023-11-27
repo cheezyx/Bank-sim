dfc5ebb08dd90348e6771091dec55fef47e5cf43
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2023-11-27 20:29:58 +0200

    Nyt login toimii

--- my-app/src/pages/LoginPage.js
+++ my-app/src/pages/LoginPage.js
@@ -3,54 +3,70 @@ import React, { useState } from 'react';
 function LoginPage() {
     const [cardId, setCardId] = useState('');
     const [pin, setPin] = useState('');
-  
+    const [message, setMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
     const handleSubmit = async (event) => {
       event.preventDefault();
+      setIsLoading(true);
       try {
-        const response = await fetch('/login', { // Oletetaan että backendisi on samassa osoitteessa
+        const response = await fetch('http://localhost:3000/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({ card_id: cardId, pin_hashed: pin }),
         });
-        const data = await response.json();
-        if (data) {
-          // Käsittely onnistuneen kirjautumisen jälkeen, esim. tokenin tallennus ja uudelleenohjaus
+    
+        if (response.ok) {
+          const token = await response.text(); // Käytä text-metodia vastauksen lukemiseen
+          if (token && token !== 'false') {
+            localStorage.setItem('token', token);
+            setMessage('Login successful');
+            // TODO: Ohjaa käyttäjä eteenpäin tai päivitä tilaa
+          } else {
+            setMessage('Password incorrect or user does not exist');
+          }
         } else {
-          // Virheenkäsittely, jos kirjautuminen ei onnistu
+          setMessage('An error occurred');
         }
       } catch (error) {
         console.error('Kirjautumisessa tapahtui virhe:', error);
+        setMessage('An error occurred during login');
+      } finally {
+        setIsLoading(false);
       }
     };
 
-  return (
-    <div>
-      <h1>Kirjaudu sisään</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Kortin ID:
-          <input 
-            type="text" 
-            value={cardId} 
-            onChange={(e) => setCardId(e.target.value)} 
-            required 
-          />
-        </label>
-        <label>
-          PIN-koodi:
-          <input 
-            type="password" 
-            value={pin} 
-            onChange={(e) => setPin(e.target.value)} 
-            required 
-          />
-        </label>
-        <button type="submit">Kirjaudu</button>
-      </form>
-    </div>
-  );
+    return (
+      <div>
+        <h1>Kirjaudu sisään</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Kortin ID:
+            <input 
+              type="text" 
+              value={cardId} 
+              onChange={(e) => setCardId(e.target.value)} 
+              required 
+            />
+          </label>
+          <label>
+            PIN-koodi:
+            <input 
+              type="password" 
+              value={pin} 
+              onChange={(e) => setPin(e.target.value)} 
+              required 
+            />
+          </label>
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? 'Loading...' : 'Kirjaudu'}
+          </button>
+        </form>
+        {message && <p>{message}</p>}
+      </div>
+    );
 }
 
 export default LoginPage;
