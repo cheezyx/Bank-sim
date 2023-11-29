ae12f7d0d6607ba382eb8d609b44ac50d8ea75bf
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2023-11-29 18:27:54 +0200

    Tätä tarvitaan tuota web-käyttöliittymää varten

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001',
+}));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
