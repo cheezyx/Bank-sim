a3cefde80b3d3a2476387ee85bdeca3c9452ca79
cheezyx <cheezyx@users.noreply.github.com>
2023-11-29 12:23:16 +0200

    dll testi

---
+++ DLLapi/dllapi.cpp
@@ -0,0 +1,3 @@
+#include "dllapi.h"
+
+DLLapi::DLLapi() {}
---
+++ DLLapi/dllapi.h
@@ -0,0 +1,12 @@
+#ifndef DLLAPI_H
+#define DLLAPI_H
+
+#include "DLLapi_global.h"
+
+class DLLAPI_EXPORT DLLapi
+{
+public:
+    DLLapi();
+};
+
+#endif // DLLAPI_H
