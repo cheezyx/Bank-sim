6fdd4b71a3da7a21de37878109c14962281fc8b6
JoonasSal <122926329+JoonasSal@users.noreply.github.com>
2023-11-30 22:53:13 +0200

    Homepage otettu pois käytöstä ja nyt aloitetaan suoraan loginpagelta

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
+//import HomePage from './pages/HomePage';
 import AutomatPage from './pages/AutomatPage';
 import CardTypeSelection from './pages/CardTypeSelection';
-// Muut import-lauseet
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/automat" element={<AutomatPage />} />
         <Route path="/cardTypeSelection" element={<CardTypeSelection />} />
